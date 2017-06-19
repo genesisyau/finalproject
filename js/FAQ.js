ef8c718cc4bb757ed95303ddc9da02dfd62c3173
104590052 <ksda5994128@ntut.org.tw>
2017-06-20 01:52:11 +0800

    add   FAQ-zh &&en .html

---
+++ js/FAQ.js
@@ -0,0 +1,34 @@
+function myFunction1() {
+    var x = document.getElementById('myDIV1');
+    if (x.style.display === 'block') {
+        x.style.display = 'none';
+    } else {
+        x.style.display = 'block';
+    }
+}
+
+function myFunction2() {
+    var x = document.getElementById('myDIV2');
+    if (x.style.display === 'block') {
+        x.style.display = 'none';
+    } else {
+        x.style.display = 'block';
+    }
+}
+
+function myFunction3() {
+    var x = document.getElementById('myDIV3');
+    if (x.style.display === 'block') {
+        x.style.display = 'none';
+    } else {
+        x.style.display = 'block';
+    }
+}
+function myFunction4() {
+    var x = document.getElementById('myDIV4');
+    if (x.style.display === 'block') {
+        x.style.display = 'none';
+    } else {
+        x.style.display = 'block';
+    }
+}
