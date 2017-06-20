35b29225333d5f3119db4ffc9b8befc0845c34fd
Genesis <genesisyau@hotmail.com>
2017-06-21 06:31:15 +0800

    fixed button once again

--- js/teaching.js
+++ js/teaching.js
@@ -1,6 +1,6 @@
-function myFunction() {
+function myFunction1() {
 var x = document.getElementById('garden');
-    if (x.style.display !== 'none') {
+    if (x.style.display === 'none') {
         x.style.display = 'block';
     } else {
         x.style.display = 'none';
