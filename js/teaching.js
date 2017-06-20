42ce4a9dfd2f59b803414a35f0d2c354ce5b003f
Genesis <genesisyau@hotmail.com>
2017-06-21 06:28:44 +0800

    fix button teaching

---
+++ js/teaching.js
@@ -0,0 +1,17 @@
+function myFunction() {
+var x = document.getElementById('garden');
+    if (x.style.display !== 'none') {
+        x.style.display = 'block';
+    } else {
+        x.style.display = 'none';
+    }
+}
+
+function myFunction2() {
+var x = document.getElementById('craft');
+    if (x.style.display === 'none') {
+        x.style.display = 'block';
+    } else {
+        x.style.display = 'none';
+    }
+}
