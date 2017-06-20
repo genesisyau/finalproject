3f0e26bd834cd4e361032b63a6a83af708553ab5
ckfx1995 <ckfx1995@gmail.com>
2017-06-21 07:06:24 +0800

    DEBUG

--- js/login.js
+++ js/login.js
@@ -57,4 +57,40 @@ $(document).ready(function(){
         }
                 
     });
-});
+                  var $messageField = $('#messageInput');
+                  var $messageList = $('#example-messages');
+                  // LISTEN FOR KEYPRESS EVENT
+                  $messageField.keypress(function (e) {
+                                         if (e.keyCode == 13) {
+                                         //FIELD VALUES
+                                         
+                                         var message = $messageField.val();
+                                         console.log(message);
+                                         
+                                         //SAVE DATA TO FIREBASE AND EMPTY FIELD
+                                         dbRef.push({text:message});
+                                         $messageField.val('');
+                                         }
+                                         });
+                  
+                  // Add a callback that is triggered for each chat message.
+                  dbRef.limitToLast(10).on('child_added', function (snapshot) {
+                                           //GET DATA
+                                           var data = snapshot.val();
+                                           
+                                           var message = data.text;
+                                           
+                                           //CREATE ELEMENTS MESSAGE & SANITIZE TEXT
+                                           var $messageElement = $("<li>");
+                                           var $nameElement = $("<strong class='example-chat-username'></strong>");
+                                           
+                                           $messageElement.text(message).prepend();
+                                           
+                                           //ADD MESSAGE
+                                           $messageList.append($messageElement)
+                                           
+                                           //SCROLL TO BOTTOM OF MESSAGE LIST
+                                           $messageList[0].scrollTop = $messageList[0].scrollHeight;
+                                           });
+                  });
+
